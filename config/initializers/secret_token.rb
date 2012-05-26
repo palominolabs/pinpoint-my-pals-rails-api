b55bf526323dae22da5f7f5745c70ab689017b76
Andrew Mitchell <andrew@palominolabs.com>
2012-05-26 12:19:26 -0700

    Adding initial rails app with heroku, devise, and omniauth gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PinpointMyPalsRailsApi::Application.config.secret_token = '0585ae6755974538998b166aab48867e7d4e5fa694b58eca1e7b2ae5112d3d9088b35e2307a31be5f417dece3ecf237eb4248abd7d033778ebd173aab411f501'
